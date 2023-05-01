6e5f90ff0af3a1a1c1ec69fc765ade2e6f4043d4
Millenmark <millenmark.iv@gmail.com>
2023-05-01 14:38:50 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,10 +4,17 @@ import mongoose from 'mongoose'
 //App config
 const app = express()
 const port = process.env.PORT || 5000
+const connectionUrl = process.env.ATLAS_URL 
+
 
 //Middleware
 
 //DB Config
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
 
 //API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello Dating App"))
