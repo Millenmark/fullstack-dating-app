258e8982599dbacf86847aa9c4dcbf50180f05ab
Millenmark <millenmark.iv@gmail.com>
2023-05-01 13:42:00 +0800

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/Header'
 export { default as DatingCards } from './DatingCards/DatingCards'
+export { default as SwipeButtons } from './SwipeButtons/SwipeButtons'
